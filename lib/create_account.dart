042a64bf26af22a9ccf5cc98864d241ec9b227f6
mobilecheng <mobilecheng@qq.com>
2019-02-24 23:00:30 +0800

    Added Start and Create Account pages.

---
+++ lib/create_account.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class CreateAccount extends StatefulWidget {
+  @override
+  _CreateAccountState createState() => _CreateAccountState();
+}
+
+class _CreateAccountState extends State<CreateAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text('Create New Account'),
+      ),
+
+      body: SafeArea(
+        child: ListView(
+          physics: new NeverScrollableScrollPhysics(),
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 50.0), // spacer - 空位符
+
+            Column(
+              children: <Widget>[
+                // TextField - Account Name
+                TextField(
+                  decoration:
+                      InputDecoration(filled: true, labelText: 'Account Name'),
+                ),
+
+                // TextField - Password
+                SizedBox(height: 30.0),
+                TextField(
+                  decoration:
+                      InputDecoration(filled: true, labelText: 'Password'),
+                ),
+
+                // TextField - Repeat Password
+                SizedBox(height: 30.0),
+                TextField(
+                  decoration: InputDecoration(
+                      filled: true, labelText: 'Repeat Password'),
+                ),
+
+                // Button - Create wallet
+                SizedBox(height: 100.0),
+                RaisedButton(
+                  child: Text('Create'),
+                  color: Colors.lightBlue,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    // TODO: next page.
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/start.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'create_account.dart';
+
+class StartPage extends StatefulWidget {
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          physics: new NeverScrollableScrollPhysics(),
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 120.0),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/LunarX_Logo.jpg'),
+                SizedBox(height: 16.0),
+                Text('LunarX_Omni Wallet'),
+
+                // Button - Create new wallet
+                SizedBox(height: 100.0),
+                RaisedButton(
+                  child: Text('Create new wallet'),
+                  color: Colors.lightBlue,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    // TODO: Show the create new wallet page.
+                    print('Current page: $context');
+                    Navigator.push(context, new MaterialPageRoute( 
+                      builder: (context) => new CreateAccount()) );
+                  },
+                ),
+
+                // Button - Restore wallet
+                SizedBox(height: 10.0),
+                RaisedButton(
+                  child: Text('   Restore wallet   '),
+                  onPressed: () {
+                    // TODO: Show the restore wallet page.
+                  },
+                ),
+              ],
+            ),
+
+            // Select language.
+
+          ],
+        ),
+      ),
+    );
+  }
+}
