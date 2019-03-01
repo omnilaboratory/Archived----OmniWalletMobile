2c7fa5705acd6fdafcc0d4306ae089a243f4ed58
mobilecheng <mobilecheng@qq.com>
2019-03-01 14:14:15 +0800

    Merge from zhuguojun

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:flutter/rendering.dart';
-=======
 import 'package:wallet_app/backupwallet/backup_wallet_index.dart';
->>>>>>> 7ede514ed1475d576d9bfa42103753a7c1d281d6
 
 import 'start.dart';
 import 'welcome_page_1.dart';
