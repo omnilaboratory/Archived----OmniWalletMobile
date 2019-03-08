9e1f8943ed5c62d95fc3b6772e86b35b44b404e9
ZhuGuojun <254698748@qq.com>
2019-03-08 10:10:53 +0800

    code demo for scopedmodel

---
+++ lib/demo/ScopedModelDemo.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class CounterModel extends Model{
+  int _counter=0;
+  int get counter{
+    return this._counter;
+  }
+
+  void increment(){
+    this._counter++;
+    notifyListeners();
+  }
+}
