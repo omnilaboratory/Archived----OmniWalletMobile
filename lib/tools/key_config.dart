f0c588b0cd9925917c8389ead0cf03a3baccac7a
ZhuGuojun <254698748@qq.com>
2019-05-09 19:32:59 +0800

    本地序列化的key md5

--- lib/tools/key_config.dart
+++ lib/tools/key_config.dart
@@ -1,18 +1,20 @@
+import 'Tools.dart';
+
 /// Key Config page.
 /// [author] Kevin Zhang
 /// [time] 2019-4-19
 
 class KeyConfig {
-  static const String user_mnemonic = 'user.mnemonic';
-  static const String user_mnemonicSeed = 'user.mnemonicSeed';
-  static const String user_mnemonic_md5 = 'user.mnemonic_md5';
-  static const String user_login_token = 'user.login_token';
-  static const String user_pinCode_md5 = 'user.pinCode_md5';
-  static const String is_backup = 'is_backup';
-  static const String backParentId = 'backParentId';
-  static const String set_language = 'set_language';
-  static const String set_currency_unit = 'set_currency_unit';
-  static const String set_theme = 'set_theme';
+  static final String user_mnemonic = Tools.convertMD5Str('user.mnemonic');
+  static final String user_mnemonicSeed = Tools.convertMD5Str('user.mnemonicSeed');
+  static final String user_mnemonic_md5 = Tools.convertMD5Str('user.mnemonic_md5');
+  static final String user_login_token = Tools.convertMD5Str('user.login_token');
+  static final String user_pinCode_md5 = Tools.convertMD5Str('user.pinCode_md5');
+  static final String is_backup = Tools.convertMD5Str('is_backup');
+  static final String backParentId = Tools.convertMD5Str('backParentId');
+  static final String set_language = Tools.convertMD5Str('set_language');
+  static final String set_currency_unit = Tools.convertMD5Str('set_currency_unit');
+  static final String set_theme = Tools.convertMD5Str('set_theme');
   static const String languageEn = 'English';
   static const String languageCn = '简体中文';
   static const String cny = 'CNY';
